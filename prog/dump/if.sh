f10c00de6fe87f3390a5e92db9f1663312929fb3
romariorobby <romariorobby@yahoo.com>
2022-09-19 10:04:56 +0700

    dump

---
+++ prog/dump/if.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# TODO: Check ucode
+
+
+# TODO: Check chassis
+#1.  if laptop intel procs:
+#  xorg-xbacklight
+#  xf86-video-intel
+#  intel-ucode
+#  currently works laptop: lenovo t440p 
+

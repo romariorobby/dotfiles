7fc5fae1fcde739568e88c521f601a40bc4081e5
romariorobby <romariorobby@yahoo.com>
2022-10-10 00:21:46 +0700

    dwmblocks: add blocks

---
+++ home/private_dot_local/bin/blocks/executable_sync-blockn.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+. sb-config
+
+# Program that intergrated with dwmblocks
+# - sxhkd
+# - blocks/
+
+dwm_dir="$HOME/.local/src/dwmf"
+dwmblocks_dir="$HOME/.local/src/dwmblocks-async"
+
+blocks_changenum(){
+	grep -q "$1 $2$" "$dwmblocks_dir"/blockn.h && printf "%s[$2] up to date\n" "$1" ||\
+		(sed -Ei "s/($1)\s([0-9]+)/\1 $2/" "$dwmblocks_dir"/blockn.h && printf "%s\n" "set $1 -> $2") || printf "Cannot set: %s\n" "$1"
+}
+
+if [ -d "$dwmblocks_dir" ]; then
+	# Sync statubar.hook (archlinux)
+	if [ -f "$dwmblocks_dir/statusbar.hook" ]; then 
+		(grep -q "\-RTMIN+$BLOCKN_PACKAGE" "$dwmblocks_dir/statusbar.hook" && printf "%s\n" "statusbar.hook[$BLOCKN_PACKAGE] up to date") \
+			|| { printf "%s\n" "Update statusbar.hook -> $BLOCKN_PACKAGE"; sed -i "s/\(-RTMIN+\)\([0-9]\+\)/\1$BLOCKN_PACKAGE/" "$dwmblocks_dir"/statusbar.hook; }
+	fi
+	# Sync 
+
+	blocks_changenum "BLOCKN_BATTERY" "$BLOCKN_BATTERY"
+	blocks_changenum "BLOCKN_VOLUME" "$BLOCKN_VOLUME"
+	blocks_changenum "BLOCKN_PACKAGE" "$BLOCKN_PACKAGE"
+	blocks_changenum "BLOCKN_CLOCK" "$BLOCKN_CLOCK"
+	blocks_changenum "BLOCKN_NEWS" "$BLOCKN_NEWS"
+
+fi
